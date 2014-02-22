5fdd79148489fb5ab696b9994104c4fe2502c113
Erik Svensson <erik.public@gmail.com>
2014-02-22 14:01:44 +0100

    Moved radio handling into the new radio.lua.

---
+++ radio.lua
@@ -0,0 +1,160 @@
+-- Radio package handling
+--
+-- (C) 2014 Erik Svensson <erik.public@gmail.com>
+-- Licensed under MIT
+--
+
+require("nrf24")
+require("decode")
+require("time")
+
+
+Radio = { nrf = nil }
+
+function Radio:open(spi_master, spi_device, ce_pin)
+	local nrf = Nrf24:create(spi_master, spi_device, ce_pin)
+	obj = nil
+	if nrf ~= nil then
+		obj = obj or {}
+		setmetatable(obj, self)
+		self.__index = self
+		self.nrf = nrf
+		self.nrf:setup(1, 250, 0, "serv1", "clie1", 32, 1, 5, 2000)
+	else
+		error("failed to create Nrf24 object")
+	end
+	return obj
+end
+
+function Radio:version()
+	return string.format("libnrf24 %d.%d.%d (%s)", nrf24_version())
+end
+
+function Radio:close()
+	self.nrf:destroy()
+end
+
+function Radio:power_up()
+	self.nrf:power_up()
+	nrf24_msleep(2) -- let the radio power up
+end
+
+function Radio:power_down()
+	self.nrf:power_down()
+end
+
+function Radio:query()
+	local result
+	local tx_data = {0x01} -- 0x01 means query
+	local ready
+	local start
+	local time
+	local diff
+	local rx_data
+	local rx_data_len = 0
+	ready = false
+	count = 0
+	-- send query, with retry
+	start = useconds_now()
+	repeat
+		result = self.nrf:send(tx_data)
+		if result < 0 then
+			time = useconds_now()
+			diff = time - start
+			if (diff >= 20000) then
+				break;
+			else
+				nrf24_usleep(1000) -- wait some
+			end
+		else
+			break;
+		end
+	until true
+	if result < 0 then
+		error("Failed to send message")
+	end
+	-- we seem to be required to wait some here otherwise start_listen seems
+	-- to fail
+	nrf24_usleep(700)
+	-- start listening for reply
+	self.nrf:start_listen()
+	-- wait some, the response seems to ready after ~6 ms at earliest
+	nrf24_msleep(6)
+	start = useconds_now()
+	while ready == false do
+		ready = self.nrf:data_ready()
+		if not ready then
+			time = useconds_now()
+			diff = time - start
+			if diff >= 200000 then
+				break;
+			else
+				nrf24_msleep(5) -- wait some
+			end
+		end
+	end
+	rx_data_len = 0
+	if ready then
+		rx_data, rx_data_len = self.nrf:receive()
+	end
+	self.nrf:stop_listen()
+	-- parse the response if any
+	if rx_data_len > 0 then
+		state, arg1, arg2 = Radio:handle_package(rx_data, rx_data_len)
+		if state == 'device_data' then
+			return true, arg1, arg2
+		elseif state == 'device_error' then
+			error(string.format('Device Error: %04x', arg1))
+		elseif state == 'error_command' then
+			error(string.format('Remote Unknown Command: %02x\n', arg1))
+		elseif state == 'error' then
+			error(string.format('Error: %02x\n', arg1))
+		elseif state == 'error_unknown_command' then
+			error(string.format('Unknown Command: %02x\n', arg1))
+		else
+			return false
+		end
+	else
+		error("Failed to receive response")
+	end
+end
+
+function Radio:handle_package(package_data, package_data_length)
+	local data = package_data
+	local len = package_data_length
+	local offset = 0
+	local command = decode_uint8(data, len)
+	offset = offset + 1
+	len = len - 1
+	if command == 0x81 then
+		local code = decode_int16(data+offset, len)
+		offset = offset + 2
+		len = len - 2
+		if code == 0x0000 then
+			local device = decode_uint64(data+offset, len)
+			offset = offset + 8
+			len = len - 8
+			local value = decode_int32(data+offset, len)
+			offset = offset + 4
+			len = len - 4
+			real_value = value / 1000000.0
+			return 'device_data', device, real_value
+		elseif code == 0x000f then
+			return 'device_end'
+		else
+			error(string.format('Device Error: %04x', code))
+		end
+	elseif command == 0xff then
+		local code = decode_int16(data+offset, len)
+		offset = offset + 2
+		len = len - 2
+		if code == 12 then
+			local received_command = decode_uint8(data+offset, len)
+			return 'error_command', received_command
+		else
+			return 'error', code
+		end
+	else
+		return 'error_unknown_command', command
+	end
+end
--- server.lua
+++ server.lua
@@ -1,122 +1,36 @@
-require("nrf24")
-require("decode")
+require("radio")
 require("time")
+require("nrf24")
 
-function handle_package(package_data, package_data_length)
-	local data = package_data
-	local len = package_data_length
-	local offset = 0
-	local command = decode_uint8(data, len)
-	offset = offset + 1
-	len = len - 1
-	if command == 0x81 then
-		local code = decode_int16(data+offset, len)
-		offset = offset + 2
-		len = len - 2
-		if code == 0x0000 then
-			local device = decode_uint64(data+offset, len)
-			offset = offset + 8
-			len = len - 8
-			local value = decode_int32(data+offset, len)
-			offset = offset + 4
-			len = len - 4
-			real_value = value / 1000000.0
-			return 'device_data', device, real_value
-		elseif code == 0x000f then
-			return 'device_end'
-		else
-			return 'device_error', code
-		end
-	elseif command == 0xff then
-		local code = decode_int16(data+offset, len)
-		offset = offset + 2
-		len = len - 2
-		if code == 12 then
-			local received_command = decode_uint8(data+offset, len)
-			return 'error_command', received_command
-		else
-			return 'error', code
-		end
-	else
-		return 'error_unknown_command', command
-	end
-end
+local radio = Radio:open(0, 0, 25)
 
-print(string.format("loaded libnrf24 %d.%d.%d (%s)", nrf24_version()))
-
-local nrf = Nrf24:create(0, 0, 25)
-if nrf ~= nil then
-	local result = nrf:setup(1, 250, 0, "serv1", "clie1", 32, 1, 5, 2000)
-	if result == 0 then
-		local tx_data = {0x01}
-		local ready
-		local start
-		local rx_start
-		local time
-		local sec_time
-		local diff
-		local rx_data
-		local rx_data_len = 0
-		local more
-		while true do
-			nrf:power_up()
-			nrf24_msleep(2)
-			start = useconds_now()
-			repeat
-				time = useconds_now()
-				sec_time = time / 1000000.0
+if radio ~= nil then
+	print(radio:version())
+	radio:power_up()
+	local start
+	local diff
+	local more
+	local success = false
+	local device
+	local measurement
+	while true do
+		start = useconds_now()
+		repeat
+			success, more, device, measurement = pcall(Radio.query, radio)
+			if success then
+				nrf24_print('%08llx, %.4f\n', device, measurement)
+			else
+				print(more)
 				more = false
-				ready = false
-				repeat
-					result = nrf:send(tx_data)
-					nrf24_usleep(900)
-				until result >= 0
-				nrf:start_listen()
-				rx_start = useconds_now()
-				while ready == false do
-					ready = nrf:data_ready()
-					time = useconds_now()
-					diff = time - rx_start
-					if diff >= 200000 then
-						break;
-					end
-				end
-				sec_time = time / 1000000.0
-				rx_data_len = 0
-				if ready then
-					rx_data, rx_data_len = nrf:receive()
-				end
-				nrf:stop_listen()
-				nrf24_usleep(500)
-				if rx_data_len > 0 then
-					state, arg1, arg2 = handle_package(rx_data, rx_data_len)
-					if state == 'device_data' then
-						nrf24_print('%.3f %08llx, %.4f\n', sec_time, arg1, arg2)
-						more = true
-					elseif state == 'device_error' then
-						nrf24_print('%.3f Device Error: %04x\n', sec_time, arg1)
-					elseif state == 'error_command' then
-						nrf24_print('%.3f Remote Unknown Command: %02x\n', sec_time, arg1)
-					elseif state == 'error' then
-						nrf24_print('%.3f Error: %02x\n', sec_time, arg1)
-					elseif state == 'error_unknown_command' then
-						nrf24_print('%.3f Unknown Command: %02x\n', sec_time, arg1)
-					end
-				else
-					nrf24_print('%.3f Timeout\n', sec_time)
-					nrf24_msleep(5)
-				end
-			until more == false
-			nrf:power_down()
-			repeat
-				diff = useconds_now() - start
-				nrf24_msleep(100)
-			until diff > 1000000
-		end
-	else
-		print('failed to setup nrf24 ' .. result)
+			end
+		until more == false
+		repeat
+			diff = useconds_now() - start
+			nrf24_msleep(100)
+		until diff > 1000000
 	end
-	nrf:destroy()
+	radio:power_down()
+	radio:close()
 else
 	print('failed to open nrf24')
 end
